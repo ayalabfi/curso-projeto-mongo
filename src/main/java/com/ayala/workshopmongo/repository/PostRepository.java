babb583b4babd2c46f50d393ca1ef6ea246747cd
Ayala Borba Filho <ayalaborbafilho@gmail.com>
2026-07-24 17:13:11 -0300

    Multi-criteria query

--- src/main/java/com/ayala/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/ayala/workshopmongo/repository/PostRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
 import java.util.List;
 
 @Repository
@@ -22,4 +23,12 @@ public interface PostRepository extends MongoRepository<Post, String> {
     // Query Method para obter titulos contendo um texto específico
     List<Post> findByTitleContainingIgnoreCase(String text);
 
-}
+    /*
+    Todas as Querys foram tiradas do doc. do MongoDB.
+    and: serve para pegar 2 informações e comparar oque estiver entre (gte = maior ou igual | lte = menor ou igual)
+    or: opção para pegar uma das opções dentro da chave
+     */
+    @Query("{ $and: [ { date: [$gte: ?1] }, { date: { $lte: ?2 } }, { $or: [ { 'title': { $regex: ?0, $options: 'i' } }, { 'body': { $regex: ?0, $options: 'i' } }, { 'comments.text': { $regex: ?0, $options: 'i' } } ] } ] }")
+    List<Post> fullSearch(String text, Date minDate, Date maxDate);
+
+    }
